5db186e56dc8a44a0693c8266173e8c94d44711c
Viktharien <viktharien@zoho.com>
2018-07-02 19:04:29 +0000

    gen sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,19 @@
 class SessionsController < ApplicationController
-  def new
+  def create
+      user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+          session[:user_id] = user.id 
+          flash.now[:success] = 'Logged in!'
+          redirect_to "/"
+      else
+          flash.now[:danger] = 'There was something wrong with your credentials.'
+          render 'new'
+      end
   end
 
   def destroy
+      session[:user_id] = nil
+      flash.now[:success] = 'Logged out!'
+      redirect_to root_path
   end
 end
