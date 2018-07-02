89f55888b1e8a6d9c6dc7c423aff2eaa4a8e523d
Viktharien <viktharien@zoho.com>
2018-07-02 17:43:38 +0000

    set up users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
+  before_action :set_user
+  
   def edit
+    
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to '/'
+    else
+      render 'edit'
+    end
   end
+  
+  private
+  
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
