5f8409bc135a0bb75b16c2ed838b3ec4106bd8da
Viktharien <viktharien@zoho.com>
2018-07-02 19:36:10 +0000

    set up session routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [:create, :destroy]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'logout', to: 'sessions#destroy'
   resources :users, only: [:edit, :update]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
