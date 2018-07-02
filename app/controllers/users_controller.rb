5710e1ef87597f0fa8f3d0da52826ab57017a768
Viktharien <viktharien@zoho.com>
2018-07-02 17:44:08 +0000

    set up users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,4 +18,8 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
+    
+    def user_params
+      params.permit(:name, :email, :password_digest)
+    end
 end
