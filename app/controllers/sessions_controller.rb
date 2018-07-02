36071675b78f2296ace7f61cfd1d451f3f7ffd47
Viktharien <viktharien@zoho.com>
2018-07-02 20:27:29 +0000

    set up and test flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,17 +8,17 @@ class SessionsController < ApplicationController
       user = User.find_by(email: params[:session][:email].downcase)
       if user && user.authenticate(params[:session][:password])
           session[:user_id] = user.id 
-          flash[:success] = 'Logged in!'
+          flash[:notice] = 'Logged in!'
           redirect_to "/"
       else
-          flash[:danger] = 'There was something wrong with your credentials.'
+          flash[:alert] = 'There was something wrong with your credentials.'
           render 'new'
       end
   end
 
   def destroy
       session[:user_id] = nil
-      flash[:success] = 'Logged out!'
+      flash[:notice] = 'Logged out!'
       redirect_to "/"
   end
 end
