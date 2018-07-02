796c3b6d4786fc40d323c543ed88f002e1e19dcb
Viktharien <viktharien@zoho.com>
2018-07-02 19:04:39 +0000

    gen sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,6 @@ class SessionsController < ApplicationController
   def destroy
       session[:user_id] = nil
       flash.now[:success] = 'Logged out!'
-      redirect_to root_path
+      redirect_to "/"
   end
 end
