20e9e0598f8278b6593b9ea2418a8dad4a352c89
Viktharien <viktharien@zoho.com>
2018-07-02 19:03:41 +0000

    gen sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/destroy'
   get 'users/edit'
   get 'users/update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
