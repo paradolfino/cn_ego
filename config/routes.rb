4c5ddff870ddc8ec94b8d1b6a1828ac9cf3eb6d7
Viktharien <viktharien@zoho.com>
2018-07-03 16:25:40 +0000

    set up participant controller with methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  get 'participants/index'
-  get 'participants/show'
-  get 'participants/new'
-  get 'participants/edit'
   root 'statics#home'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
   get 'update/admin/:id', to: 'users#edit'
   resources :users, only: [:update]
-  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
