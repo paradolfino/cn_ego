1e26bfaa84653945d1b7dfb354fc0285aa163ad1
Viktharien <viktharien@zoho.com>
2018-07-03 15:25:24 +0000

    fix users controller and update view to allow updating of password

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_02_163855) do
+ActiveRecord::Schema.define(version: 2018_07_03_152203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "participants", force: :cascade do |t|
+    t.string "name"
+    t.integer "points"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
