3940ebe9865f4db5d9984db29ad4494b78affa50
Viktharien <viktharien@zoho.com>
2018-07-06 14:18:33 +0000

    add validations for participant

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,6 @@
 class Participant < ApplicationRecord
     extend FriendlyId
     friendly_id :name, use: :slugged
+    
+    validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 30 }
 end
