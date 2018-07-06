c3a267d7d4f7c950fdb2ce2df05d2230993d8da2
Viktharien <viktharien@zoho.com>
2018-07-06 14:20:54 +0000

    add validations for participant

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -3,4 +3,6 @@ class Participant < ApplicationRecord
     friendly_id :name, use: :slugged
     
     validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 30 }
+    validates :points, presence: true
+    validates_numericality_of :points, only_integer: true
 end
