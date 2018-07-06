89fe14abe4a8dada1831549b26b8c394a73407ea
Viktharien <viktharien@zoho.com>
2018-07-06 13:56:58 +0000

    add authenticate user before action to controllers

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,5 +1,6 @@
 class ParticipantsController < ApplicationController
   before_action :set_participant, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user, except: [:show]
   
   def index
     @participants = Participant.order("points DESC")
