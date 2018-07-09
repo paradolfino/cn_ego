69bbd893ea98419625863ade593151e9681e4577
Viktharien <viktharien@zoho.com>
2018-07-09 14:33:35 +0000

    setup javascript to inc/dec points based on value specified in text field

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'update/admin/:id', to: 'users#edit'
   resources :users, only: [:update]
   resources :participants
+  get 'participants/increment', to 'participants#inc'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
