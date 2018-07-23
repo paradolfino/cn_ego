622b50f2128bca4211d333ef416e76ecbf9cef6e
Viktharien <viktharien@zoho.com>
2018-07-23 21:02:40 +0000

    add more  user model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,18 @@ RSpec.describe User, type: :model do
     it "should validate presence of password" do
         expect(build(:invalid_password)).to be_invalid
     end
+    
+    it "should validate length of password" do
+        user = build(:user)
+        puts user.password.length
+        expect(user.password.length).to be >= 3
+    end
+    
+    it "should validate uniqueness of email" do
+        user = create(:user)
+        user2 = build(:user)
+        expect(user2).to_not be_valid
+    end
   
   end
   
