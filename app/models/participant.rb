09eba21ba9be40469533169b06538c5fa4b6b8d3
Viktharien <viktharien@zoho.com>
2018-07-23 20:05:43 +0000

    add csv import method to participant model

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,5 @@
 class Participant < ApplicationRecord
+    require 'csv'
     extend FriendlyId
     friendly_id :name, use: :slugged
     
@@ -8,5 +9,11 @@ class Participant < ApplicationRecord
     
     def should_generate_new_friendly_id?
        slug.blank? || name_changed?
+    end
+    
+    def self.import(file)
+        CSV.foreach(file.path, headers: true, header_converters: :symbol) do |row|
+            Participant.create!(row.to_hash)
+        end
     end
 end
