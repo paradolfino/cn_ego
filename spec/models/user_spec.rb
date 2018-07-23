a79b456e5fb7616eb76f49b669cae694fff3f526
Viktharien <viktharien@zoho.com>
2018-07-23 20:47:08 +0000

    create user factory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,11 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context User do
     it "should validate presence of email" do
-        user = User.new(password: "test").save
-        expect(user).to be_invalid
+        expect(build(:invalid_email)).to be_invalid
     end
     
     it "should validate presence of password" do
-        user = User.new(email: "test@test.com").save
-        expect(user).to be_invalid
+        expect(build(:invalid_password)).to be_invalid
     end
   
   end
