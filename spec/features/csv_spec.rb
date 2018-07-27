f58f6d47689f7482494de0fd7cc591395c80ed6a
viktharien <viktharien@zoho.com>
2018-07-27 12:19:02 -0500

    write import csv method and pass csv feature spec

--- spec/features/csv_spec.rb
+++ spec/features/csv_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe ParticipantsController, type: :controller do
 
     it "redirects to the home page" do
       get :import
-      expect(response).to redirect_to root_path
+      expect(response).to redirect_to participants_path
     end
 
     it "redirects to the home page after post" do
       allow(Participant).to receive(:import).with("foo.csv")
       post :import, params: {file: "foo.csv"}
-      expect(response).to redirect_to root_path
+      expect(response).to redirect_to participants_path
     end
 
     it "adds a flash notice" do
