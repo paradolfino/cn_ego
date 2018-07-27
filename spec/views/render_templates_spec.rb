51857d6d0d690408166e226fc7225d5a22ef5887
viktharien <viktharien@zoho.com>
2018-07-27 13:21:41 -0500

    write template render tests for views

--- spec/views/render_templates_spec.rb
+++ spec/views/render_templates_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe ParticipantsController, type: :controller do
 
-  context ""
+  it "renders index template"
+
+  it "renders new template"
+
+  it "renders edit template"
+
+  it "renders show template"
 
 end
