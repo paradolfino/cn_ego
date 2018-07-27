eca725544bf49a49ba9a002855964901b90aaa45
viktharien <viktharien@zoho.com>
2018-07-27 13:43:52 -0500

    write template render tests for views

--- spec/views/render_templates_spec.rb
+++ spec/views/render_templates_spec.rb
@@ -27,3 +27,31 @@ RSpec.describe ParticipantsController, type: :controller do
   end
 
 end
+
+RSpec.describe ParticipantsController, type: :controller do
+
+  it "renders index template" do
+    get :index
+    expect(response).to render_template :index
+  end
+
+  it "renders new template" do
+    get :new
+    expect(response).to render_template :new
+  end
+
+  it "renders edit template" do
+    participant = create(:participant)
+    get :edit, params: { id: participant.to_param }
+
+    expect(response).to render_template :edit
+  end
+
+  it "renders show template" do
+    participant = create(:participant)
+    get :show, params: { id: participant.to_param }
+
+    expect(response).to render_template :show
+  end
+
+end
