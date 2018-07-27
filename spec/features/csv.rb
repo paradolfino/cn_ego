9bb836afb9caacc6561c4cc92a5e0b8862c17e0d
Viktharien Volander <viktharien@Apples-MacBook-Pro-3.local>
2018-07-26 21:09:24 -0500

    begin add csv feature test

---
+++ spec/features/csv.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+describe ParticipantsController do
+  context "POST #import" do
+    it "redirects to the home page" do
+      allow(Participant).to receive(:import).with("foo.csv")
+      post :import, file: "foo.csv"
+      expect(response).to redirect_to root_path
+    end
+
+    it "adds a flash notice" do
+      allow(Participant).to receive(:import).with("foo.txt")
+      post :import, file: "foo.csv"
+      expect(flash[:notice]).to eq "Participant was successfully imported."
+    end
+
+    it "imports the csv file" do
+      expect(Participant).to receive(:import).with("foo.csv")
+      post :import, file: "foo.csv"
+    end
+  end
+end
