191b64c6acc08fc4cd0ddb87083569ad277f0590
viktharien <viktharien@zoho.com>
2018-08-02 10:28:21 -0400

    write coverage tests for participants controller : pijzwu

--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -16,4 +16,12 @@ RSpec.describe ParticipantsController, type: :controller do
     end
   end
 
+  describe "GET #show" do
+    it "returns http success" do
+      part = build(:participant)
+      get :show, params: {id: part.to_param}
+      expect(response).to have_http_status(:success)
+    end
+  end
+
 end
