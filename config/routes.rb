d451e25671bd84716760cad376ed0a4513cc0e5b
viktharien <viktharien@zoho.com>
2018-08-02 17:25:14 -0400

    write update action coverage tests : rasnps

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get 'admin/logout', to: 'sessions#destroy', as: "logout"
   get 'admin/edit', to: 'users#edit'
   post 'admin/edit', to: 'users#update'
-  patch 'participants/:id/update', to: 'participants#update'
+  get 'participants/:id/edit', to: 'participants#edit'
+  patch 'participants/:id/edit', to: 'participants#update'
   resources :participants do
     collection { post :import}
   end
