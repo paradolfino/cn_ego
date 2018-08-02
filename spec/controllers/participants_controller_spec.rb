a712dce7e2fc64eeeb6dfdc14f3804ce10722745
viktharien <viktharien@zoho.com>
2018-08-02 17:28:44 -0400

    write update action coverage tests : bzklmg

--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -83,7 +83,7 @@ RSpec.describe ParticipantsController, type: :controller do
     end
 
     it "renders edit template on failure to update participant" do
-      patch :update, params: {participant: invalid_attributes}
+      patch :update, params: {id: participant.to_param,participant: new_attributes}
       expect(response).to render_template :edit
     end
 
