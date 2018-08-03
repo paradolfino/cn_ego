29d2d4be24a6569e4d97378f2bd6626804ff4b61
viktharien <viktharien@zoho.com>
2018-08-03 11:44:26 -0400

    write coverage tests for inc action : ddxjge

--- spec/controllers/participants_controller_spec.rb
+++ spec/controllers/participants_controller_spec.rb
@@ -143,7 +143,7 @@ RSpec.describe ParticipantsController, type: :controller do
     end
     it "renders a flash message on increment" do
       get :inc, params: { id: participant.to_param, amount: 10 }
-
+      
     end
   end
 
