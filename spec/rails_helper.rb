cf1130659f022e36cd222b05d88c57bb96a393d0
viktharien <viktharien@zoho.com>
2018-08-06 10:47:55 -0400

    configure capybara to work with devise : xpmjds

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'support/database_cleaner'
+require 'support/warden'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
