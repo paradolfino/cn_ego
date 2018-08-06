f4ea529e86ad1a1d3f0fc89b97c8f4ebd95f32d1
viktharien <viktharien@zoho.com>
2018-08-06 11:28:22 -0400

    write integration test for participants index - test inc/dec buttons : xfqhpy

--- spec/integrations/participants_spec.rb
+++ spec/integrations/participants_spec.rb
@@ -16,7 +16,7 @@ feature "participants/index" do
     visit participants_path
     fill_in 'points', with: 1000
     click_button('+')
-    expect(participant.points)
+    expect(participant.points).to eq(1000)
   end
 end
 
