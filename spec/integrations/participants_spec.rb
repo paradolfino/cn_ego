c5d946ee79b19e991eaa90e6651c8bbdefec38be
viktharien <viktharien@zoho.com>
2018-08-06 11:28:07 -0400

    write integration test for participants index - test inc/dec buttons : jndybu

--- spec/integrations/participants_spec.rb
+++ spec/integrations/participants_spec.rb
@@ -14,7 +14,7 @@ feature "participants/index" do
   scenario "renders a list of participants and allows for inc points" do
 
     visit participants_path
-    fill_in ''
+    fill_in 'points', with: 1000
     click_button('+')
     expect(participant.points)
   end
