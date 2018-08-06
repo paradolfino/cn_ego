ef4df579fcdc109fcd5cf039fa56674c864a4385
viktharien <viktharien@zoho.com>
2018-08-06 09:31:31 -0400

    continue writing integration tests for participants spec : frnlnp

--- spec/integrations/participants_spec.rb
+++ spec/integrations/participants_spec.rb
@@ -11,3 +11,23 @@ feature "participants/index" do
     expect(page).to have_content("test2")
   end
 end
+
+feature 'New Participant' do
+  scenario 'user adds a new Participant' do
+    user = create(:user)
+    visit participants_path
+
+    expect{
+      click_link 'New Task'
+      fill_in 'Name', with: 'Learn Rspec'
+      fill_in 'Priority', with: 1
+      fill_in 'Due date', with: DateTime.now
+      select(user.email, from: 'task_user_id')
+      click_button('Create Task')
+    }.to change(Task, :count).by(1)
+
+    expect(current_path).to eq(task_path(Task.last.id))
+    expect(page).to have_content('Learn Rspec')
+
+  end
+end
