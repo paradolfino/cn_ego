99f14379ae6b3cef050e245ac6211d3a36a27f95
viktharien <viktharien@zoho.com>
2018-08-07 09:41:12 -0400

    write tests for reward model : mpkkcl

--- spec/factories/rewards.rb
+++ spec/factories/rewards.rb
@@ -4,25 +4,25 @@ FactoryBot.define do
     cost 2000
     img "image string"
 
-    factory :invalid_name do
+    factory :invalid_reward_name do
       name nil
       cost 1000
       img "so much for images"
     end
 
-    factory :invalid_cost do
+    factory :invalid_reward_cost do
       name "Reward Three"
       cost nil
       img "omg an image"
     end
 
-    factory :invalid_img do
+    factory :invalid_reward_img do
       name "Reward Four"
       cost 100
       img nil
     end
 
-    factory :invalid_number do
+    factory :invalid_reward_number do
       name "Reward Five"
       cost "10"
       img "imager"
