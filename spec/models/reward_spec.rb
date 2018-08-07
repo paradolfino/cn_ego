e02eda2f1fdcc185b7d503000be23c66da2de573
viktharien <viktharien@zoho.com>
2018-08-07 09:41:42 -0400

    write tests for reward model : tiqzab

--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 RSpec.describe Reward, type: :model do
   context "validations" do
     it "should validate presence of name" do
-      expect(build(:invalid_name)).to be_invalid
+      expect(build(:invalid_reward_name)).to be_invalid
     end
 
     it "should validate presence of cost" do
-      expect(build(:invalid_cost)).to be_invalid
+      expect(build(:invalid_reward_cost)).to be_invalid
     end
 
     it "should validate presence of image" do
-      expect(build(:invalid_cost)).to be_invalid
+      expect(build(:invalid_reward_img)).to be_invalid
     end
 
     it "should validate numericality" do
-      expect(build(:invalid_number)).to be_invalid
+      expect(build(:invalid_reward_number)).to be_invalid
     end
 
   end
