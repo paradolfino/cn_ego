cf70b3e256bcd69f7c26507866ef9e5acc420fe0
viktharien <viktharien@zoho.com>
2018-08-07 09:34:38 -0400

    write tests for reward model : ikatqi

--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Reward, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context Reward do
+    it "should validate presence of name" do
+      reward = build(:invalid_name)
+      expect(reward).to be_invalid
+    end
+
+    it "should validate presence of points" do
+      reward = build(:invalid_points)
+      expect(reward).to be_invalid
+    end
+
+    it "should be a number" do
+      reward = Reward.create(name: "able", points: 1)
+      expect(reward).to be_valid
+    end
+
+  end
+
 end
