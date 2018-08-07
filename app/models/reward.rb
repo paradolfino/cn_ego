9ee2403403323c1453012b57906c239971ae52b1
viktharien <viktharien@zoho.com>
2018-08-07 09:43:26 -0400

    write passing model validations for reward : izzgju

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,2 +1,8 @@
 class Reward < ApplicationRecord
+
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 30 }
+  validates :cost, presence: true
+  validates :img, presence: true
+  validates_numericality_of :cost, only_integer: true
+
 end
