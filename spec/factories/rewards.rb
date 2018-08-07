7c8faab5b1e87df08669a859650040fd01b1561d
viktharien <viktharien@zoho.com>
2018-08-07 09:31:04 -0400

    write rewards factory : srwoeh

--- spec/factories/rewards.rb
+++ spec/factories/rewards.rb
@@ -3,5 +3,21 @@ FactoryBot.define do
     name "Reward One"
     cost 2000
     img "image string"
+
+    factory :invalid_name do
+      name nil
+      cost 1000
+      img "so much for images"
+    end
+
+    factory :invalid_cost do
+      name "Reward Three"
+      cost nil
+      img "omg an image"
+    end
+
+    factory :invalid_img do
+
+    end
   end
 end
