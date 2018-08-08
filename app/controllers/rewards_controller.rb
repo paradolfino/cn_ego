7b9fcaae18d2b22667900590dcb7312aa953150e
viktharien <viktharien@zoho.com>
2018-08-08 10:49:47 -0400

    write passing controller code for rewards : tsmbuy

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -12,5 +12,14 @@ class RewardsController < ApplicationController
 
   def create
     @reward = Reward.new(reward_params)
+    if @reward.save
+    else
+      @errors = []
+      @reward.errors.full_messages.each do |m|
+        @errors << m
+      end
+      flash[:alert] = "Please correct the following errors: #{@errors.join(". ")}"
+      redirect_to new_reward_path
+    end
   end
 end
