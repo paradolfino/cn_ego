14313b3eeb92d1f0dcd8a28433d669931dac799a
viktharien <viktharien@zoho.com>
2018-08-08 10:37:29 -0400

    generate rewards controller : vnhxqt

--- spec/controllers/rewards_controller_spec.rb
+++ spec/controllers/rewards_controller_spec.rb
@@ -24,99 +24,100 @@ RSpec.describe RewardsController, type: :controller do
         get :show, params: {id: reward.to_param}
         expect(assigns(:reward)).to eq(reward)
       end
-      describe "GET #new" do
-        it "returns http success" do
-          get :new
-          expect(response).to have_http_status(:success)
-        end
-
-        it "assigns @reward to a new Reward" do
-          part = create(:reward)
-          get :new, params: {id: part.to_param}
-          expect(assigns(:reward)).to be_a_new(Reward)
-        end
+    end
+    describe "GET #new" do
+      it "returns http success" do
+        get :new
+        expect(response).to have_http_status(:success)
       end
 
-      describe "POST #create" do
+      it "assigns @reward to a new Reward" do
+        part = create(:reward)
+        get :new, params: {id: part.to_param}
+        expect(assigns(:reward)).to be_a_new(Reward)
+      end
+    end
 
-        let(:reward) {create(:reward)}
-        let(:valid_attributes) { attributes_for(:reward )}
-        let(:invalid_attributes) { attributes_for(:invalid_reward_name)}
+    describe "POST #create" do
 
-        it "creates a new reward" do
-          expect{
-            post :create, params: {reward: valid_attributes}
-          }.to change(Reward, :count).by(1)
-        end
+      let(:reward) {create(:reward)}
+      let(:valid_attributes) { attributes_for(:reward )}
+      let(:invalid_attributes) { attributes_for(:invalid_reward_name)}
 
-        it "redirects on save" do
+      it "creates a new reward" do
+        expect{
           post :create, params: {reward: valid_attributes}
-          expect(response).to redirect_to(rewards_path)
-        end
+        }.to change(Reward, :count).by(1)
+      end
 
-        it "renders a flash message on save" do
-          post :create, params: {reward: valid_attributes}
-          expect(flash[:notice]).to be_present
-        end
+      it "redirects on save" do
+        post :create, params: {reward: valid_attributes}
+        expect(response).to redirect_to(rewards_path)
+      end
 
-        it "renders a flash message on failure to save" do
-          post :create, params: {reward: invalid_attributes}
-          expect(flash[:alert]).to be_present
-        end
+      it "renders a flash message on save" do
+        post :create, params: {reward: valid_attributes}
+        expect(flash[:notice]).to be_present
+      end
 
-        it "fails to create a new reward" do
+      it "renders a flash message on failure to save" do
+        post :create, params: {reward: invalid_attributes}
+        expect(flash[:alert]).to be_present
+      end
 
-          expect(build(:invalid_both)).to be_invalid
+      it "fails to create a new reward" do
 
-        end
-
-        it "redirects to new template on failure to save new reward" do
-          post :create, params: {reward: invalid_attributes}
-          expect(response).to redirect_to(new_reward_path)
-        end
+        expect(build(:invalid_both)).to be_invalid
 
       end
 
-      describe "POST #update" do
+      it "redirects to new template on failure to save new reward" do
+        post :create, params: {reward: invalid_attributes}
+        expect(response).to redirect_to(new_reward_path)
+      end
 
-        let(:reward) {create(:reward)}
-        let(:valid_attributes) { attributes_for(:reward )}
-        let(:new_attributes) { attributes_for(:updated_reward)}
-        let(:invalid_attributes) { attributes_for(:invalid_reward_name)}
+    end
 
-        it "updates a reward" do
-          patch :update, params: {id: reward.to_param,reward: new_attributes}
-          reward.reload
-          expect(reward.name).to eq("Updated Reward")
-          expect(reward.img).to eq("Updated Image")
-          expect(reward.cost).to eq(800)
-        end
+    describe "POST #update" do
 
-        it "redirects on update" do
-          patch :update, params: {id: reward.to_param,reward: new_attributes}
-          expect(response).to redirect_to(rewards_path)
-        end
+      let(:reward) {create(:reward)}
+      let(:valid_attributes) { attributes_for(:reward )}
+      let(:new_attributes) { attributes_for(:updated_reward)}
+      let(:invalid_attributes) { attributes_for(:invalid_reward_name)}
 
-        it "renders a flash message on update" do
-          patch :update, params: {id: reward.to_param,reward: new_attributes}
-          expect(flash[:notice]).to be_present
-        end
+      it "updates a reward" do
+        patch :update, params: {id: reward.to_param,reward: new_attributes}
+        reward.reload
+        expect(reward.name).to eq("Updated Reward")
+        expect(reward.img).to eq("Updated Image")
+        expect(reward.cost).to eq(800)
+      end
 
-        it "renders a flash message on failure to update" do
-          patch :update, params: {id: reward.to_param,reward: invalid_attributes}
-          expect(flash[:alert]).to be_present
-        end
+      it "redirects on update" do
+        patch :update, params: {id: reward.to_param,reward: new_attributes}
+        expect(response).to redirect_to(rewards_path)
+      end
 
-        it "fails to update reward" do
+      it "renders a flash message on update" do
+        patch :update, params: {id: reward.to_param,reward: new_attributes}
+        expect(flash[:notice]).to be_present
+      end
 
-          expect(build(:invalid_both)).to be_invalid
+      it "renders a flash message on failure to update" do
+        patch :update, params: {id: reward.to_param,reward: invalid_attributes}
+        expect(flash[:alert]).to be_present
+      end
 
-        end
+      it "fails to update reward" do
 
-        it "redirects to edit template on failure to update reward" do
-          patch :update, params: {id: reward.to_param,reward: invalid_attributes}
-          expect(response).to redirect_to(edit_reward_path(reward))
-        end
+        expect(build(:invalid_both)).to be_invalid
 
       end
+
+      it "redirects to edit template on failure to update reward" do
+        patch :update, params: {id: reward.to_param,reward: invalid_attributes}
+        expect(response).to redirect_to(edit_reward_path(reward))
+      end
+
+    end
 end
