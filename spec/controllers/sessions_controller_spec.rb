d57e68545c8f791fed8f623300de1a78a0b1250d
viktharien <viktharien@zoho.com>
2018-08-13 16:30:06 -0400

    write tests for create : vrtxqh

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -12,8 +12,8 @@ RSpec.describe SessionsController, type: :controller do
   describe "POST #create session" do
     it "creates a new session" do
       user = create(:user)
-      controller.session[:user_id] = user.id
-      
+      post :create, params: {session: {}}
+
     end
   end
 
