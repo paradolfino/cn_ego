79424985285e9db4f74faddbf9d85c48611630bb
viktharien <viktharien@zoho.com>
2018-08-13 15:59:00 -0400

    write new test for sessions controller spec : wwbwvm

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
   describe "GET #new session" do
+
+  end
+
+  describe "POST #create session" do
+
+  end
+
+  describe "DELETE #destroy session" do
     
   end
 end
