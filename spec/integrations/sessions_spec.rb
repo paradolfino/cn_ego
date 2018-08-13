6e056c86f97cd7599f1f7af50ec6e652e655bf14
viktharien <viktharien@zoho.com>
2018-08-13 17:10:36 -0400

    write integrations test for new session : ahxrsj

--- spec/integrations/sessions_spec.rb
+++ spec/integrations/sessions_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature "admin/login" do
   scenario "it renders login form" do
     visit login_path
-    puts page
+    puts page.inspect
     expect(page).to have_content("Log In")
   end
 
