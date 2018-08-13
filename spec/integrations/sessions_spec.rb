1f2bfec7bd77e2e00a895dbc3c910b02dc0c11d6
viktharien <viktharien@zoho.com>
2018-08-13 17:33:04 -0400

    write integrations test for new session : nnmfvs

--- spec/integrations/sessions_spec.rb
+++ spec/integrations/sessions_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 feature "admin/login" do
   scenario "it renders login form" do
     visit login_path
-    puts page.body
     expect(page).to have_xpath("//form")
   end
 
@@ -14,6 +13,7 @@ feature "admin/login" do
     fill_in "session_email", with: user.email
     fill_in "session_password", with: user.email
     click_button "Log In"
+    puts page.body
     expect(page).to be_participants_page
   end
 
