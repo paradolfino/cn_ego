0c281b81d61ea62d9a6e2aa44487fe3e83843db1
viktharien <viktharien@zoho.com>
2018-08-13 17:12:22 -0400

    write integrations test for new session : vdlcev

--- spec/integrations/sessions_spec.rb
+++ spec/integrations/sessions_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 feature "admin/login" do
   scenario "it renders login form" do
     visit login_path
-    puts page.inspect
+    puts page.body
     sleep(3)
     expect(page).to have_content("Admin Login")
   end
