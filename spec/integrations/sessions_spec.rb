4b35d9823fc6d287f74e3e6098d111ab65dfb626
viktharien <viktharien@zoho.com>
2018-08-13 17:30:53 -0400

    write integrations test for new session : tntscx

--- spec/integrations/sessions_spec.rb
+++ spec/integrations/sessions_spec.rb
@@ -14,7 +14,7 @@ feature "admin/login" do
     fill_in "session_email", with: user.email
     fill_in "session_password", with: user.email
     click_button "Log In"
-    field = page.find("//table")
+    field = page.find(:xpath, "//table")
     expect(page).to have_content("Test")
   end
 
