6c6d7c64c8b51ea00bde08e75eb5f5c869857fc4
viktharien <viktharien@zoho.com>
2018-08-13 16:11:55 -0400

    write new test for sessions controller spec : iohrwo

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
   describe "GET #new session" do
+    it "returns a 200 http status" do
+      
+    end
 
   end
 
@@ -10,6 +13,6 @@ RSpec.describe SessionsController, type: :controller do
   end
 
   describe "DELETE #destroy session" do
-    
+
   end
 end
