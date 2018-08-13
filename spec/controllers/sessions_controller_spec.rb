61562eca2f584b2c27880343954ce14693a8d998
viktharien <viktharien@zoho.com>
2018-08-13 16:49:37 -0400

    write tests for create : liqzbe

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -30,7 +30,6 @@ RSpec.describe SessionsController, type: :controller do
     end
 
     it "fails to create new session with invalid credentials" do
-      user = create(:user)
       post :create, params: {session: {email: user.email, password: "not correct"}}
       expect(controller.session[:user_id]).to be_nil
     end
