5b101df109d931752f436925d14e5d63df41c05c
viktharien <viktharien@zoho.com>
2018-08-15 11:03:56 -0500

    add integration tests for index and new views

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -4,7 +4,6 @@ class Reward < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true, length: { minimum: 3, maximum: 30 }
   validates :cost, presence: true
-  validates :img, presence: true
   validates_numericality_of :cost, only_integer: true
 
 end
---
+++ spec/integrations/rewards_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+feature "rewards/index" do
+  scenario "it renders a list of rewards" do
+    create(:reward)
+    create(:second_reward)
+    visit rewards_path
+    expect(page).to have_content("Reward One")
+    expect(page).to have_content("10")
+  end
+
+end
+
+feature "rewards/new" do
+  scenario "it renders a new reward form" do
+    visit rewards_path
+
+    expect{
+      click_link 'New'
+      fill_in 'reward_name', with: 'Test Reward'
+      fill_in 'reward_cost', with: 10
+      page.attach_file('reward_img', Rails.root.join('public', 'apple-touch-icon.png'))
+      click_button('Create Reward')
+    }.to change(Reward, :count).by(1)
+
+    expect(current_path).to eq(rewards_path)
+    expect(page).to have_content('Test Reward')
+  end
+
+end
